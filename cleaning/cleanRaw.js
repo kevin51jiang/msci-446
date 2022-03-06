38ba47208dbf3bc0d342acea204fd07106bc463b
Kevin Jiang <kevin51jiang@email.com>
2022-03-06 16:37:54 -0500

    a

---
+++ cleaning/cleanRaw.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const fileNames = [
+  "gamelist01.json",
+  "gamelist02.json",
+  "gamelist03.json",
+  "gamelist04.json",
+];
+
+// relative to root directory
+const rawDir = "./data/raw/";
+
+try {
+  let allData = [];
+
+  fileNames.map((filename) => {
+    const data = fs.readFileSync(rawDir + filename, "utf-8");
+    const jsonified = JSON.parse(data);
+    allData = [...allData, ...jsonified];
+  });
+
+  fs.writeFileSync(rawDir + "allGames.json", JSON.stringify(allData));
+
+  console.log(allData);
+} catch (err) {
+  console.error(err);
+}
